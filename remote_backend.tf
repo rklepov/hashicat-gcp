2b1e18a4d396380b47fe73aa63e686bef16a6d2c
Roman Klepov <rklepov@gmail.com>
2021-11-03 17:50:39 +0000

    Added vpc module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "roman-gcp-training"
     workspaces {
       name = "hashicat-gcp"
--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,36 @@
 module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+  source  = "terraform-google-modules/network/google"
+  version = "~> 3.0"
 
-    project_id   = var.project
-    network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
+  project_id   = var.project
+  network_name = "gaurav-network"
+  routing_mode = "GLOBAL"
 
-    subnets = [
-        {
-          subnet_name   = "gaurav-subnet"
-          subnet_ip     = "10.100.10.0/24"
-          subnet_region = var.region
-        }
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+
+
+  secondary_ranges = {
+    gaurav-subnet = [
+      {
+        range_name    = "gaurav-subnet-secondary-01"
+        ip_cidr_range = "192.168.64.0/24"
+      }
     ]
+  }
+
+  routes = [
+    {
+      name              = "egress-internet"
+      description       = "route through IGW to access internet"
+      destination_range = "0.0.0.0/0"
+      tags              = "egress-inet"
+      next_hop_internet = "true"
+    }
+  ]
 }
